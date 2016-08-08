68276da859584923f309269b5029f0d1b2a1ee63
Richard Marbach <rmarbach@gmail.com>
2016-08-08 16:59:40 +0200

    Cleaned up scrollglue

--- src/app/shared/scroll-glue.directive.ts
+++ src/app/shared/scroll-glue.directive.ts
@@ -1,4 +1,4 @@
-import { Directive, AfterContentInit, OnDestroy, ElementRef, HostListener } from '@angular/core';
+import { Directive, AfterContentInit, OnDestroy, ElementRef, HostListener} from '@angular/core';
 
 @Directive({
   selector: '[scrollGlue]'
@@ -7,26 +7,22 @@ export class ScrollGlueDirective implements AfterContentInit, OnDestroy {
   private el: any;
   private isLocked = true;
   private _observer: MutationObserver;
-  private _oldScrollHeight = 0;
 
   constructor(el: ElementRef) {
     this.el = el.nativeElement;
    }
 
    @HostListener('scroll') onScroll() {
-     const percent = (this.el.scrollHeight / 100);
-     if (this.el.scrollHeight - this.el.scrollTop > (10 * percent)) {
-       this.isLocked = true;
-     } else {
-       this.isLocked = false;
-     }
+     const percent = (this.el.scrollHeight * 0.1);
+
+     // Unless close to the very bottom it should always jump down
+     this.isLocked = (this.el.scrollHeight - this.el.scrollTop) > percent;
    }
 
    ngAfterContentInit() {
      this.el.scrollTop = this.el.scrollHeight;
      this._observer = new MutationObserver(mutations => {
        if (this.isLocked) {
-         this._oldScrollHeight = this.el.scrollHeight;
          this.el.scrollTop = this.el.scrollHeight;
        }
      });
