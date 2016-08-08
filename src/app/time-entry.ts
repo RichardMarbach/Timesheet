38b75e63ab8dd61824486f0a18d82d0879310ea1
Richard Marbach <rmarbach@gmail.com>
2016-08-08 12:01:42 +0200

    Better timestring parsing

--- src/app/time-entry.ts
+++ src/app/time-entry.ts
@@ -22,26 +22,63 @@ export class TimeEntry {
     set startTime(time) {
         if (time === this._startTime) { return; }
 
-        if (typeof time === 'string') {
-            const times = time.split(':');
-            this._startTime = new TimeUnit(+times[0], +times[1]);
-        } else {
+        if (time instanceof TimeUnit) {
             this._startTime = time;
+        } else {
+            this._startTime = parseTimeString(time).adjustTo24Hour();
         }
     }
 
     set endTime(time) {
         if (time === this._endTime) { return; }
 
-        if (typeof time === 'string') {
-            const times = time.split(':');
-            this._endTime = new TimeUnit(+times[0], +times[1]);
-        } else {
+        if (time instanceof TimeUnit) {
             this._endTime = time;
+        } else {
+            this._endTime = parseTimeString(time).adjustTo24Hour();
         }
     }
 
     elapsedTime(): TimeUnit {
         return this.endTime.sub(this.startTime).adjustTo24Hour();
     }
+}
+
+function parseTimeString(input: any) {
+    // Make sure it's an input we can work with
+    if (typeof input !== 'string'
+        || /[^0-9:]/.test(input)
+        || input.length === 0) {
+        return new TimeUnit();
+    }
+
+    // HH:MM
+    if (input.indexOf(':') !== -1) {
+        const times = input.split(':');
+        // :MM
+        if (input.indexOf(':') === 0) {
+            return new TimeUnit(0, +times[1].slice(0, 2));
+        }
+
+        // HH:
+        if (!times[1]) {
+            return new TimeUnit(+times[0].slice(0, 2), 0);
+        }
+
+        // HH:MM
+        return new TimeUnit(+times[0].slice(0, 2), +times[1].slice(0, 2));
+    }
+
+    // H or HH
+    if (input.length === 2 || input.length === 1) {
+        return new TimeUnit(+input);
+    }
+
+    // HMM
+    if (input.length === 3) {
+        return new TimeUnit(+input[0], +input.slice(1));
+    }
+
+    // HHMM
+    return new TimeUnit(+input.slice(0, 2), +input.slice(2, 4));
 }
